855842892a95bb0a5496c132b438b8e50c7d0403
Saleem Hadad <saleem.hadad@talabat.com>
2022-01-30 23:58:43 +0400

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('finance:refresh')->onOneServer()->everyFifteenMinutes();
-        $schedule->command('finance:report')->onOneServer()->lastDayOfMonth('23:00');
+        // $schedule->command('finance:report')->onOneServer()->lastDayOfMonth('23:00');
     }
 
     /**
