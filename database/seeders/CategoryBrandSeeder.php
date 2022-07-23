48a03fc433b5be7ff14e215d99f427d4f1b85a4c
Saleem Hadad <saleem.hadad@talabat.com>
2022-07-24 02:07:28 +0400

    wip

--- database/seeders/CategoryBrandSeeder.php
+++ database/seeders/CategoryBrandSeeder.php
@@ -14,7 +14,6 @@ class CategoryBrandSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
         $income = Category::create(['name' => 'Income', 'type' => Category::INCOME]);
         $income->brands()->create(['name' => 'Salary'])->transactions()->createMany([
             [
@@ -138,53 +137,5 @@ class CategoryBrandSeeder extends Seeder
                 'created_at' => now()->subDays(100)
             ]
         ]);
-=======
-        Category::create(['name' => 'Income', 'type' => Category::INCOME, 'color' => 'red'])
-            ->brands()
-            ->create(['name' => 'Salary']);
-
-        Category::create(['name' => 'Housing', 'type' => Category::EXPENSES, 'color' => 'blue'])
-            ->brands()
-            ->create(['name' => 'House Rent']);
-
-        Category::create(['name' => 'Groceries', 'type' => Category::EXPENSES, 'color' => 'green'])
-            ->brands()
-            ->createMany([
-                ['name' => 'LULU'],
-                ['name' => 'CARREFOUR'],
-            ]);
-
-        Category::create(['name' => 'Utilities', 'type' => Category::EXPENSES, 'color' => 'orange'])
-            ->brands()
-            ->createMany([
-                ['name' => 'Smart Dubai'],
-            ]);
-
-        Category::create(['name' => 'Transportation', 'type' => Category::EXPENSES, 'color' => 'purple'])
-            ->brands()
-            ->createMany([
-                ['name' => 'ENOC'],
-            ]);
-
-        Category::create(['name' => 'Shopping', 'type' => Category::EXPENSES, 'color' => 'pink'])
-            ->brands()
-            ->createMany([
-                ['name' => 'IKEA'],
-                ['name' => 'HOME CENTRE'],
-                ['name' => 'MCDONALDS'],
-            ]);
-
-        Category::create(['name' => 'Support', 'type' => Category::EXPENSES, 'color' => 'indigo'])
-            ->brands()
-            ->createMany([
-                ['name' => 'Family Support'],
-            ]);
-
-        Category::create(['name' => 'Debt', 'type' => Category::EXPENSES, 'color' => 'gray'])
-            ->brands()
-            ->createMany([
-                ['name' => 'Debt'],
-            ]);
->>>>>>> main
     }
 }
